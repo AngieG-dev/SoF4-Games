d93d0bd8f94e87af17c0f909900e94e0ff11f9b0
Constanza Riquelme <constanza.ri@gmail.com>
2026-05-07 18:19:25 -0400

    chore(frontend): add frontend structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from 'react-router-dom' // Agregar Route cuando se necesite
+
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+              {/* Las rutas se agregan aquí en sprints siguientes */}
+          </Routes>
+      </BrowserRouter>
+  )
+}
+
+export default App
